c62a5e30392f93ccaefe1d03d2c39275a624d27e
Renan Araujo <renanaraujo852@hotmail.com>
2023-04-02 21:25:58 -0300

    feat: send message: Ola spring

---
+++ src/main/java/com/api/olaspring/OlaspringApplication.java
@@ -0,0 +1,13 @@
+package com.api.olaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/olaspring/OlaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.olaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
